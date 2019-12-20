34d7a562605f36295c7a5df46ef7184cc2a6cd76
QAQxiaotongtong <52728015+QAQxiaotongtong@users.noreply.github.com>
2019-12-20 10:40:03 +0800

    123

--- 12.20/12.20/Program.cs
+++ 12.20/12.20/Program.cs
@@ -10,7 +10,17 @@ namespace _12._20
     {
         static void Main()
         {
-           int asdas=453453453;
+            
         }
+    }
+    public class Person
+    {
+        public Person(string fName,string lName)
+        {
+            this.FirstName = fName;
+            this.LastName = lName;
+        }
+        public string FirstName;
+        public string LastName;
     }
 }
