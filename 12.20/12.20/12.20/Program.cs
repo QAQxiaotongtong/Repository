ab465078c01d622ff6e555f0961b50865e44a96a
QAQxiaotongtong <52728015+QAQxiaotongtong@users.noreply.github.com>
2019-12-20 10:52:25 +0800

    Add files via upload

---
+++ 12.20/12.20/12.20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._20
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+    public class Person
+    {
+        public Person(string fName,string lName)
+        {
+            this.FirstName = fName;//111
+            this.LastName = lName;
+        }
+        public string FirstName;
+        public string LastName;
+    }
+}
